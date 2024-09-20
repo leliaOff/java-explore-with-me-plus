7630b949f9fdc1599086088381b91a4c2ad8d7c9
Elena Polukova <e.brykova@yofin.ru>
2024-09-20 17:01:16 +0400

    fixed: checkstyle

--- stat/stat-server/src/test/java/ru/practicum/server/hit/HitServiceTest.java
+++ stat/stat-server/src/test/java/ru/practicum/server/hit/HitServiceTest.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.dto.HitDto;
