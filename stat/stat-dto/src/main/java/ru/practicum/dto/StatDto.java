3c4b1593840f364858ee1ad6b50e4e61670be4eb
Elena Polukova <e.brykova@yofin.ru>
2024-09-20 10:05:58 +0400

    Тесты для контроллера и сервиса получения статистики

--- stat/stat-dto/src/main/java/ru/practicum/dto/StatDto.java
+++ stat/stat-dto/src/main/java/ru/practicum/dto/StatDto.java
@@ -11,4 +11,12 @@ public class StatDto {
     private String app;
     private String uri;
     private Long hits;
+
+    public boolean equals(Object o) {
+        if (getClass() != o.getClass()) return false;
+        StatDto statDto = (StatDto) o;
+        return this.app.equals(statDto.app)
+                && this.uri.equals(statDto.uri)
+                && this.hits.equals(statDto.hits);
+    }
 }
