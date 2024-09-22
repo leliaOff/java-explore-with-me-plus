41266067a3d3240f7daf120fe85efdba2730deec
Elena Polukova <e.brykova@yofin.ru>
2024-09-22 12:43:45 +0400

    fixed: java.lang.String cannot be converted to java.time.LocalDateTime

--- ewm/src/main/java/ru/practicum/ewm/MainApplication.java
+++ ewm/src/main/java/ru/practicum/ewm/MainApplication.java
@@ -6,11 +6,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 import ru.practicum.dto.HitDto;
 import ru.practicum.ewm.client.StatClient;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(MainApplication.class, args);
         StatClient statClient = context.getBean(StatClient.class);
-        statClient.hit(new HitDto(null, "app", "uri", "ip", "2020-05-05 00:00:00"));
+        statClient.hit(new HitDto(null, "app", "uri", "ip", LocalDateTime.parse("2020-05-05 00:00:00")));
     }
 }
