bd0d108c74df3f23ea3675e5104b1dcae54dbcea
Max <maxusjob@ya.ru>
2024-09-25 17:47:11 +0200

    user admin controller implementation

--- ewm/src/main/java/ru/practicum/ewm/dto/user/NewUserRequest.java
+++ ewm/src/main/java/ru/practicum/ewm/dto/user/NewUserRequest.java
@@ -1,13 +1,21 @@
 package ru.practicum.ewm.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class NewUserRequest {
+    @NotBlank
+    @Email
+    @Length(min = 6, max = 254)
     private String email;
+    @NotBlank
+    @Length(min = 2, max = 250)
     private String name;
 }
