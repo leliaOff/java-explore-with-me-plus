817a17742d236647a614e982ddf6328d3347fce4
Max <maxusjob@ya.ru>
2024-09-26 18:18:08 +0200

    code style fix

--- ewm/src/main/java/ru/practicum/ewm/constraints/EventDateConstraint.java
+++ ewm/src/main/java/ru/practicum/ewm/constraints/EventDateConstraint.java
@@ -9,10 +9,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Constraint(validatedBy = EventDateValidator.class)
-@Target( { ElementType.METHOD, ElementType.FIELD })
+@Target({ElementType.METHOD, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface EventDateConstraint {
     String message() default "Date must be in future +2 hours.";
+
     Class<?>[] groups() default {};
+
     Class<? extends Payload>[] payload() default {};
 }
